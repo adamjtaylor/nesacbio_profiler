4fb4a7576a180ac9fc1eba6e60e490ed92ce3e24
Adam J Taylor <tayloraj@uw.edu>
2015-10-05 14:48:54 -0700

    Partially comments server.R

--- server.R
+++ server.R
@@ -18,220 +18,242 @@ library(pracma) # for the error function
 library(fBasics) # for the heavyside function
 
 # Open shiny server
-
 shinyServer(function(input, output) {
         
-        # reactive to upload file
-        # returns uploaded file
-        Upload <- reactive ({                 
-                # Input file, initially null
-                
-                inFile <- input$file1
-                
-                
-                if (is.null(inFile))
-                        return(NULL)
-                
-                if(input$filetype == "ownfile") {
-                
-                upload <- read.table(inFile$datapath, header = TRUE) 
-                }
-                else if (input$filetype == "iontof") {
-                        peaks <- read.table(inFile$datapath, sep="\t", fill=TRUE, header=FALSE, nrows=1, skip=1) # imports masses (second row of df)
-                        
-                        upload <- read.table(inFile$datapath, 
-                                             sep="\t", fill=TRUE, 
-                                             header=FALSE, skip=3) %>%
-                                rename(peaks)
-                        peaks <- NULL # drop colnames
-                        upload[,ncol(upload)] <- NULL # drops last column which should be full of NAs
-                        
-                        colnames(upload) <- paste("X",colnames(upload), sep="")
-                        
-                        
-                        upload <- upload %>% rename(c("XNA" = "time"))
+# reactive to upload file
+# returns uploaded file
+Upload <- reactive ({                 
 
+    # Input file, initially null
+    inFile <- input$file1
+  
+    # If no file uploaded return null
+    if (is.null(inFile))
+      return(NULL)
+    
+    # If filetype is generic "ownfile" format just upload the data as is
+    if(input$filetype == "ownfile") {
+      upload <- read.table(inFile$datapath, header = TRUE)
+    }
+    
+    # If filetype is an export from Ion-ToF profiles 
+    # Then we need to do some data munging to get in a workable format
+    else if (input$filetype == "iontof") {
+            
+      # Import peaks from exported data file from 2nd row      
+      peaks <- read.table(inFile$datapath,
+                          sep="\t",
+                          fill=TRUE,
+                          header=FALSE,
+                          nrows=1,
+                          skip=1)
+      
+      # Upload data from exported file and rename columns with peaks
+      upload <- read.table(inFile$datapath,
+                           sep="\t", fill=TRUE,
+                           header=FALSE,
+                           skip=3) %>%
+              rename(peaks)
+      
+      # Drop column names as no longer needed
+      peaks <- NULL 
+      
+      # Drop the last column which should be full of NAs
+      upload[,ncol(upload)] <- NULL 
                         
-                }
-                
-               return(upload)
-        })
-        
-        # reactive to upload example data
-        # and for users to select dataset
-        # returns selected dataset
-        pickData <- reactive({
-                    
-        bl3 <- read.table("./examples/bl3.txt", header=TRUE)
-        bl6 <- read.table("./examples/bl6.txt", header=TRUE)
-        vamas <- read.table("./examples/vamas.txt", header=TRUE)
-        testdelta <- read.table("./examples/testdelta2.txt", header=TRUE, sep=",")
-        switch(input$dataset,
-               "Example 1: 6% PS/PMMA bilayer" = bl6,
-               "Example 2: 3% PS/PMMA bilayer" = bl3,
-               "Example 3: VAMAS multilayer" = vamas,
-               "Test delta layer" = testdelta,
-              "User uploaded file" = Upload()
-        )
-         })
-        
-        
-        # reactive to melt selected data into "long" format
-        GetData <- reactive({
-                
-                pickdata <- pickData()
-              
-                
-                
-                sampledata <- pickdata %>%
-               # sampledata <-  read.table("vamas_example.txt", header=TRUE) %>%
-                        melt(id.vars = "time",
-                             variable.name = "peak",
-                             value.name = "intensity")
-                return(sampledata)
-        })
-        
-        # reactive to get max time
-        # returns max time
-        MaxTime <- reactive({
-                sampledata <- GetData()
-                MaxTime <- max(sampledata$time)
-                return(MaxTime)
-        })
-        
-        # reactive to list peaks avaliable to plot
-        # returns list of peaks
-        peakList <- reactive({
-                sampledata <- GetData()
-                peakList <- sampledata %>% distinct(peak) # unique(as.character(colnames(peakSelect)))
-                peakList$time <- NULL 
-                peakList$intensity <- NULL
-                peakList$peak <- as.character(peakList$peak)
-                peakList <- peakList$peak
-                return(peakList)
-        })
-        
-        # reactive to subset data based on selected peak
-        # returns subset of data for selected peak
-        subData <- reactive({
-                sampledata <- GetData()
-                selectedPeak <- input$peak
-                peakdata <- if (is.null(input$peak)) 
-                                (subset(sampledata, peak == "X69"))
-                           else(subset(sampledata, peak == input$peak))
-                return(peakdata)
-        })
-        
-        # reactive to plot points
-        # returns ggplot of points
-        output$plotpoints <- renderPlot({
+      # Paste "X" before peake mass so that they work (won't accept numbers)
+      colnames(upload) <- paste("X",colnames(upload), sep="")
+      
+      # Rename the first column as time
+      upload <- upload %>% rename(c("XNA" = "time"))
 
+  } # close the else if
+                
+  # return the uploaded file
+  return(upload)
 
-          # 'Charactistic peaks of VAMAS samples include m/z 26, 42, 43.99, 344.2, 563.4, 43, 59.02, 231.2, 277.2, 1175.8'
-          
-          #635.5
-          
-          peakdata <- subData()
-          
-          
-          #interface <- cbind(interface$time, log(interface$intensity)) %>% as.data.frame()
-          #colnames(interface) <- c("t", "logIntensity")
-          
-          # Plot sample data
-          plotpoints <- ggplot() + 
-                  geom_point(data = peakdata,
-                             aes(time,intensity), colour = "blue") +
-                  geom_line(data = peakdata,
-                            aes(time,intensity), colour = "blue") +  
-                  #    geom_point(data = subset(peakdata, peak == "X1175.8"),
-                  #               aes(time,log(intensity)), colour = "green") +
-                  #    geom_line(data = subset(peakdata, peak == "X1175.8"),
-                  #              aes(time,log(intensity)), colour = "green") + 
-                  theme_classic() + xlim(min(input$zoomrange), max(input$zoomrange))
-          if(input$dispLog == TRUE){
-                  return(plotpoints+scale_y_log10() )
-          } else {
-          
-          return(plotpoints) }
-  })
+}) # close the reactive
+        
+# reactive to upload example data
+# and for users to select dataset
+# returns the selected dataset
+pickData <- reactive({
+  
+  # Upload example datasets
+  bl3 <- read.table("./examples/bl3.txt", header=TRUE)
+  bl6 <- read.table("./examples/bl6.txt", header=TRUE)
+  vamas <- read.table("./examples/vamas.txt", header=TRUE)
+  testdelta <- read.table("./examples/testdelta2.txt", header=TRUE, sep=",")
+  
+  # Switch between datasets as user selects them
+  switch(input$dataset,
+         "Example 1: 6% PS/PMMA bilayer" = bl6,
+         "Example 2: 3% PS/PMMA bilayer" = bl3,
+         "Example 3: VAMAS multilayer" = vamas,
+         "Test delta layer" = testdelta,
+         "User uploaded file" = Upload()
+         )
+
+}) # closes the reactive
+        
+        
+# reactive to melt selected data into "long" format
+GetData <- reactive({
+        
+  # Load the selected dataset        
+  pickdata <- pickData()
+  
+  # Melt the dataset into columns of time, peak and intensity
+  sampledata <- pickdata %>%
+                melt(id.vars = "time",
+                     variable.name = "peak",
+                     value.name = "intensity")
+  
+  return(sampledata) # Return the melted dataset
+
+}) # close the reactive
+        
+# reactive to get the maximum time / length of sputtering
+# returns maximum value in time column
+MaxTime <- reactive({
+  sampledata <- GetData()
+  MaxTime <- max(sampledata$time)
+  return(MaxTime)
+}) # close the reactive
+        
+# reactive to list peaks avaliable to plot
+# returns list of peaks
+peakList <- reactive({
+  sampledata <- GetData()
+  peakList <- sampledata %>% distinct(peak)
+  peakList$time <- NULL 
+  peakList$intensity <- NULL
+  peakList$peak <- as.character(peakList$peak)
+  peakList <- peakList$peak
+  return(peakList)
+}) # close the reactive
+        
+# reactive to subset data based on selected peak
+# returns subset of data for selected peak
+subData <- reactive({
+  sampledata <- GetData()
+  selectedPeak <- input$peak
+  peakdata <- if (is.null(input$peak))
+                 (subset(sampledata, peak == "X69"))
+              else(subset(sampledata, peak == input$peak))
+  return(peakdata)
+}) # close the reactive
+        
+# renderPlot to plot points
+# returns ggplot of points
+output$plotpoints <- renderPlot({
+  
+  # Load the subset data
+  peakdata <- subData()
+
+  # Plot sample data
+  plotpoints <- ggplot() + 
+                geom_point(data = peakdata,
+                           aes(time,intensity), 
+                           colour = "blue") +
+                geom_line(data = peakdata,
+                          aes(time,intensity), 
+                          colour = "blue") +
+                theme_classic() + 
+                xlim(min(input$zoomrange), max(input$zoomrange))
+  
+  # Scales intensity by log10 if selected by user
+  if(input$dispLog == TRUE){
+    return(plotpoints+scale_y_log10() )
+  } 
+  
+  # Otherwise returns unscaled plot
+  else {
+    return(plotpoints)
+  }
+ 
+}) # close the renderPlot
   
   
-  # reactive to subset data to brushed points
-  # and fit error function across this region
-  # returns NLS fit
-  fitData <- reactive({
-          
-          peakdata <- subData()
-          
+# reactive to subset data to brushed points
+# and fit error function across this region
+# returns NLS fit
+fitData <- reactive({
+  
+  # load the subset data        
+  peakdata <- subData()
+  
+  # Get the brushed interface and define the start and end points
+  brushedinterface <- brushedPoints(peakdata, 
+                                    input$plotpoints_brush, 
+                                    xvar="time", 
+                                    yvar="intensity")
+    
+  start <- min(brushedinterface$time) #min(input$range)
+  end <- max(brushedinterface$time) #max(input$range)
+  
+  
+  # If log10 fitting is sleected, scale the data
+  if (input$fitLog == TRUE){
+    interface <- brushedinterface %>% 
+      select(one_of(c("time", "intensity"))) %>%
+      transmute(t = time, intensity=log10(intensity))
+  }
+  
+  # Otherwise don't scale the data
+  else {
+    interface <- brushedinterface %>%
+      select(one_of(c("time", "intensity"))) %>%
+      transmute(t = time, intensity=(intensity))   
+  }
 
-                  
-
-          brushedinterface <- brushedPoints(peakdata, input$plotpoints_brush, xvar="time", yvar="intensity")
-          start <- min(brushedinterface$time) #min(input$range)
-          end <- max(brushedinterface$time) #max(input$range)
-          
-          if (input$fitLog == TRUE){
-          interface <- brushedinterface %>% 
-                  #subset(peak == "X69") %>%
-                  #subset(time<=end & time >= start) %>%
-                  select(one_of(c("time", "intensity"))) %>%
-                  transmute(t = time, intensity=log10(intensity))
-          }
-          else {
-                  interface <- brushedinterface %>% 
-                          #subset(peak == "X69") %>%
-                          #subset(time<=end & time >= start) %>%
-                          select(one_of(c("time", "intensity"))) %>%
-                          transmute(t = time, intensity=(intensity))   
-          }
-
-          
-          
-          # Write the function
-          error.func <- function(t, B, sigma, i, A) {
-                  0.5 * (B-A) * (1 + erf( (t - i) / (sigma *sqrt(2) ) ) ) + A
-                  #erf(x)v
-          }
-          
-          delta.func <- function(t, A, B, sigma, i) {
-                  (A-B) * exp(-0.5*(t - i)^2/sigma^2) + B
-          }
-          
+  # Define the error function
+  error.func <- function(t, B, sigma, i, A) {
+    0.5 * (B-A) * (1 + erf( (t - i) / (sigma *sqrt(2) ) ) ) + A
+  }
+   
+  # Write the delta layer function - not complete
+  delta.func <- function(t, A, B, sigma, i) {
+    (A-B) * exp(-0.5*(t - i)^2/sigma^2) + B
+  }
+# # Write the heavyside function  - NOT COMPLETE          
 #           heavyside.func <- function(t, A, B, i) {
 #                           (((sign(t-i) + 1)/2)*(B-A)+A)
 #           }
           
           
-          # Generate fitted data
-          #t <- seq(start,end, by=0.01)
-          #logIntensity <- error.func(seq(start, end, by=0.01), 13, 5, 45, 6)
+  # Generate fitted data
+  #t <- seq(start,end, by=0.01)
+  #logIntensity <- error.func(seq(start, end, by=0.01), 13, 5, 45, 6)
           
-          # Optimize fit
-if (input$interfacetype == "interface"){
-          nls_fit <-nls(intensity ~ error.func(t, B, sigma, i, A), 
-                        data = interface, 
-                        start = list(B = input$B, #10, # max intensity 
-                                     A = input$A, #3# baseline intensity
-                                     sigma = input$sigma, # sigma
-                                     #i = 120
-                                     i = ((end-start)/2)+start # interface position
-                        ), 
-                        trace = F)
-} else {
-        nls_fit <-nls(intensity ~ delta.func(t, B, sigma, i, A), 
-                      data = interface, 
-                      start = list(B = input$B, #10, # max intensity 
-                                   A = input$A, #3# baseline intensity
-                                   sigma = input$sigma, # sigma
-                                   #i = 120
-                                   i = ((end-start)/2)+start # interface position
-                      ), 
-                      trace = F)
-}
+  # Optimize the fit
+  
+  # If fitting an interface fit with the error function
+  if (input$interfacetype == "interface"){
+    nls_fit <-nls(intensity ~ error.func(t, B, sigma, i, A),
+                  data = interface,
+                  start = list(B = input$B, #10, # max intensity
+                               A = input$A, #3# baseline intensity
+                               sigma = input$sigma, # sigma
+                               i = ((end-start)/2)+start # x position
+                               ),
+                  trace = F)
+  }
+  # If fitting delta layer use the delta function
+  else {
+    nls_fit <-nls(intensity ~ delta.func(t, B, sigma, i, A),
+                  data = interface,
+                  start = list(B = input$B, #10, # max intensity
+                               A = input$A, #3# baseline intensity
+                               sigma = input$sigma, # sigma
+                               i = ((end-start)/2)+start # x position
+                               ),
+                  trace = F)
+  }
           
-return(nls_fit)
-          # need to add and if else statement depending on if this converged!
-  })
+  return(nls_fit) # Eeturns fit
+
+}) # close the reactive
   
   # reactive to prodict fitted data across brushed interface region
   # returns data frame of 100 predicted points
